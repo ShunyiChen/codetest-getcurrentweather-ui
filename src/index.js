cbce1d65f7de7cd728f763f045d2ac5516a12c54
Simeon Chen <simeon.chen@ericsson.com>
2021-06-10 13:27:37 +0800

    fix(index): change proxy url

--- src/index.js
+++ src/index.js
@@ -1,19 +1,19 @@
 const axios = require('axios').default;
 
 function generateHtml(arr) {
-  var html = ''; 
-  for(var i=0;i<arr.length;i++){
-    html += '<option value="'+arr[i].name+'">'+arr[i].name+'</option>';
+  var html = '';
+  for (var i = 0; i < arr.length; i++) {
+    html += '<option value="' + arr[i].name + '">' + arr[i].name + '</option>';
   }
   return html;
 }
 
 function loadCityList() {
-  axios.get('http://localhost:33472/city/findall')
-     .then (res => {
-        console.log(res)
-        s1.innerHTML = generateHtml(res.data.data);
-     }).catch(function (error) {
+  axios.get('http://8.142.15.127:8888/city-service/city/findall')
+    .then(res => {
+      console.log(res)
+      s1.innerHTML = generateHtml(res.data.data);
+    }).catch(function (error) {
       // handle error
       console.log(error);
     })
